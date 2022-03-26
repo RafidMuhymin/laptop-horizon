1cbed3e6ea4de8ca6bba026b01f238f5bdae41db
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-26 22:33:16 +0600

    responsified layout

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -7,8 +7,8 @@ export default function Shop() {
   const selectedLaptopsState = useState([]);
 
   return (
-    <div className="m-0 row">
-      <div className="m-0 col-8 mw-100 row row-cols-3">
+    <div className="m-0 row flex-column flex-xl-row">
+      <div className="m-0 col-12 col-xl-8 mw-100 row row-cols-1 row-cols-md-2 row-cols-xl-3">
         {laptopsData.map((laptop) => {
           return (
             <Laptop
@@ -20,7 +20,7 @@ export default function Shop() {
         })}
       </div>
 
-      <div className="col-4">
+      <div className="col-12 col-xl-4">
         <Cart selectedLaptopsState={selectedLaptopsState} />
       </div>
     </div>
