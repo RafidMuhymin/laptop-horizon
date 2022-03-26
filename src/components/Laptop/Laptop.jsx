f11eb22701cb3bd014acd3ad41c8f8392110ffb0
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-26 18:07:28 +0600

    added laptops

--- src/components/Laptop/Laptop.jsx
+++ src/components/Laptop/Laptop.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function Laptop() {
-  return <div>Laptop</div>;
+export default function Laptop({ laptop }) {
+  const { name, price, image } = laptop;
+
+  return (
+    <div className="laptop">
+      <img src={image} alt={name} className="mw-100" />
+      <div className="p-3 text-center">
+        <h3>{name}</h3>
+        <p className="fs-5">Price: ${price}</p>
+      </div>
+    </div>
+  );
 }
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import laptopsData from "../../data/laptops.json";
+import Laptop from "../Laptop/Laptop";
 
 export default function Shop() {
-  return <div>Shop</div>;
+  return (
+    <div className="shop m-0 mw-100 row row-cols-3">
+      {laptopsData.map((laptop) => {
+        return <Laptop key={laptop.name} laptop={laptop} />;
+      })}
+    </div>
+  );
 }
