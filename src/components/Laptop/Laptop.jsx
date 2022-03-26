2ce97e883360210f12f4aff6a7f30ea3eb26b215
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-26 19:37:29 +0600

    styled laptop

--- src/components/Laptop/Laptop.jsx
+++ src/components/Laptop/Laptop.jsx
@@ -4,12 +4,13 @@ export default function Laptop({ laptop }) {
   const { name, price, image } = laptop;
 
   return (
-    <div className="laptop">
+    <div className="d-flex flex-column">
       <img src={image} alt={name} className="mw-100" />
-      <div className="p-3 text-center">
+
+      <div className="p-3 pb-4 text-center flex-grow-1 d-flex flex-column justify-content-between">
         <h3>{name}</h3>
         <p className="fs-5">Price: ${price}</p>
-        <button className="btn btn-primary w-100 gap-1 d-inline-flex justify-content-between align-items-center">
+        <button className="btn btn-primary w-75 mx-auto gap-1 d-inline-flex justify-content-between align-items-center">
           Add to Cart
           <Icon icon="ant-design:shopping-cart-outlined" />
         </button>
