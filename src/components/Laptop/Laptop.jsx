d3bdab24ef1dd82ef507f83f5aaac676589e1294
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-26 22:22:07 +0600

    show modal for duplicate item and more than 4items

--- src/components/Laptop/Laptop.jsx
+++ src/components/Laptop/Laptop.jsx
@@ -1,12 +1,37 @@
 import { Icon } from "@iconify/react";
+import { useState } from "react";
+import Modal from "../Modals/Modal";
 
 export default function Laptop({ laptop, selectedLaptopsState }) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState(null);
+
   const { name, price, image } = laptop;
 
   const [selectedLaptops, setSelectedLaptops] = selectedLaptopsState;
 
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
   const handleAddToCart = () => {
-    if (!selectedLaptops.includes(laptop) && selectedLaptops.length < 4) {
+    if (selectedLaptops.includes(laptop)) {
+      setModalMessage(
+        <p className="text-danger fs-5">
+          You have already selected this laptop!
+        </p>
+      );
+
+      setModalIsOpen(true);
+    } else if (selectedLaptops.length === 4) {
+      setModalMessage(
+        <p className="text-center text-danger fs-5">
+          Can't select more than 4 laptops!
+        </p>
+      );
+
+      setModalIsOpen(true);
+    } else {
       setSelectedLaptops([...selectedLaptops, laptop]);
     }
   };
@@ -18,6 +43,7 @@ export default function Laptop({ laptop, selectedLaptopsState }) {
       <div className="p-3 pb-4 text-center flex-grow-1 d-flex flex-column justify-content-between">
         <h3>{name}</h3>
         <p className="fs-5">Price: ${price}</p>
+
         <button
           onClick={handleAddToCart}
           className="btn btn-primary px-3 mx-auto gap-2 d-inline-flex justify-content-between align-items-center"
@@ -26,6 +52,46 @@ export default function Laptop({ laptop, selectedLaptopsState }) {
           <Icon icon="ant-design:shopping-cart-outlined" />
         </button>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={{
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+          },
+        }}
+      >
+        {modalMessage && (
+          <div className="text-center">
+            <Icon
+              icon="clarity:error-standard-solid"
+              style={{
+                color: "red",
+                width: "4rem",
+                height: "4rem",
+                display: "block",
+                margin: "0 auto 1rem auto",
+              }}
+            />
+
+            <p className="fs-1 text-danger">Oops...</p>
+
+            {modalMessage}
+
+            <button
+              onClick={closeModal}
+              className="fs-5 btn btn-secondary px-3"
+            >
+              Close
+            </button>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
