e164561f1c9d9a05d3006bf9d94a3636e2f02445
RafidMuhymin <rafidmuhymin@gmail.com>
2022-03-26 22:40:40 +0600

    added padding to laptops container

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,7 +9,7 @@ export default function Shop() {
 
   return (
     <div className="m-0 row flex-column flex-xl-row">
-      <div className="m-0 col-12 col-xl-8 mw-100 row row-cols-1 row-cols-md-2 row-cols-xl-3">
+      <div className="m-0 py-3 col-12 col-xl-8 mw-100 row row-cols-1 row-cols-md-2 row-cols-xl-3">
         {laptopsData.map((laptop) => {
           return (
             <Laptop
